10184140b0df88206f18c8e4f972b2035025ad67
briank86 <143430894+briank86@users.noreply.github.com>
2023-10-09 00:35:56 -0400

    Update Lahman Database SQL.sql

--- Lahman Database SQL.sql
+++ Lahman Database SQL.sql
@@ -40,23 +40,3 @@ JOIN(SELECT MAX(posHR1.HR) as HR, posHR1.POS
 	 GROUP BY posHR1.POS) maxHR
 ON posHR.HR = maxHR.HR AND posHR.POS = maxHR.POS
 
-	
-/*Lahman data from 2019*/
-/*Who has the most home runs who is not in the hall of fame and not currently playing in 2019 excluding Barry Bonds and Alex Rodriguez?
-
-This would be David Ortiz at 541. 
-To go one step further who has the most home runs that is Hall of Fame eligible.
-Oritz wasn't eligible yet and same with Adrian Beltre who had 477.
-
-So the player after the 2019 season who has the most home runs who isn't in the hall of fame and is eligible would be Adam Dunn with 462 (also failed to get the 5% needed to stay on ballot in 2020)*/
-SELECT Nhr.fullName, MAX(Nhr.sumHR) AS HR
-FROM(SELECT NotHall.fullName, SUM(NotHall.HR) as sumHR
-	FROM(SELECT h.inducted, p.playerID, CONCAT(p.nameFirst, ' ' , p.nameLast) as fullName, p.finalGame, b.HR
-		FROM People AS p
-		JOIN Batting AS b ON p.playerID = b.playerID
-		LEFT JOIN HallOfFame AS h ON p.playerID = h.playerID
-		WHERE h.playerID is NULL AND CAST(SUBSTRING(p.finalGame, 0, 5) AS INT) < 2019) NotHall
-	GROUP BY NotHall.fullName) Nhr
-GROUP BY Nhr.fullName
-ORDER BY HR DESC
-
